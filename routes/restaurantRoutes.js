47353ab6c64be5ae5a54087a1cf888087fd15958
SaddamHosen42 <saddamhosen1433@gmail.com>
2025-09-25 20:53:01 +0600

    [Add]:restaurant Routes

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const restaurantController = require('../Controllers/restaurantController');
+
+// POST: Add a new restaurant
+router.post('/', restaurantController.createRestaurant);
+
+// GET: Get all restaurants
+router.get('/', restaurantController.getAllRestaurants);
+
+// GET: Search restaurants
+router.get('/search', restaurantController.searchRestaurants);
+
+// GET: Get nearby restaurants
+router.get('/nearby', restaurantController.getNearbyRestaurants);
+
+// GET: Get restaurant by ID
+router.get('/:id', restaurantController.getRestaurantById);
+
+// PUT: Update restaurant
+router.put('/:id', restaurantController.updateRestaurant);
+
+// DELETE: Delete restaurant
+router.delete('/:id', restaurantController.deleteRestaurant);
+
+// PUT: Update restaurant rating
+router.put('/:id/rating', restaurantController.updateRestaurantRating);
+
+module.exports = router;
